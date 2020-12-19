36e386d20fb63ceb3c01e02229a448f6b29801bc
Rajikotra <rajikotra@gmail.com>
2020-12-19 15:30:22 +0000

    loginpage testcases

---
+++ src/main/java/com/gic/tution_center/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.gic.tution_center.utils;
+
+public class Constants {
+
+    public static String DASHBOARD_URL = "http://demo.dontonno.com/cms-test-v2/";
+}
--- src/test/java/com/gic/tution_center/TutionRunner.java
+++ src/test/java/com/gic/tution_center/TutionRunner.java
@@ -1,10 +1,13 @@
 package com.gic.tution_center;
 
+import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features = ".")
+
 public class TutionRunner {
 
 }
